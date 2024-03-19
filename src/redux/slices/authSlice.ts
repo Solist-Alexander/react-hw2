21bc5ea52a30cff12160f69fdcaab19b6260fd7f
Solist-Alexander <fcco1233@gmail.com>
2024-03-19 12:38:42 +0200

    interceptor

--- src/redux/slices/authSlice.ts
+++ src/redux/slices/authSlice.ts
@@ -1,36 +1,60 @@
-import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
+import {createAsyncThunk, createSlice, isFulfilled, isRejected} from "@reduxjs/toolkit";
 import {IAuth} from "../../interfaces/authInterface";
 import {authService} from "../../services/authService";
 import {AxiosError} from "axios";
+import {IUser} from "../../interfaces/userInterface";
 
 interface IState {
-    
+    registerError: string
+    loginError: string;
+    currentUser: IUser
 }
 
-const initialState:IState = {
-    
-}
-const register= createAsyncThunk<void, { user: IAuth }>(
+const initialState: IState = {
+    registerError: null,
+    loginError: null,
+    currentUser: null
+};
+
+const register = createAsyncThunk<void, { user: IAuth }>(
     'authSlice/register',
-    async ({user}, {rejectWithValue})=>{
+    async ({user}, {rejectWithValue}) => {
         try {
             await authService.register(user)
-        } catch (e){
+        } catch (e) {
             const err = e as AxiosError
             return rejectWithValue(err.response.data)
         }
     }
 )
 
+
 const authSlice = createSlice({
-    name:'authSlice',
+    name: 'authSlice',
     initialState,
-    reducers:{},
+    reducers: {},
     extraReducers: builder =>
         builder
 
+            .addCase(register.rejected, state => {
+                state.registerError = 'Username already exist'
+            })
+
+            .addMatcher(isFulfilled(register), state => {
+                state.registerError = null
+                state.loginError = null
+            })
+
 })
 
-export {
+const {reducer: authReducer, actions} = authSlice;
 
+const authActions = {
+    ...actions,
+    register,
+}
+
+export {
+    authActions,
+    authReducer
 }
